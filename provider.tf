1beaec3263886ecdf0af10b4fc41578491b26910
Ayush Rawat <ayushrawat18@gmail.com>
2022-09-22 12:38:04 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  subscription_id = "${var.subscriptionId}"
-  client_id       = "${var.clientId}"
-  client_secret   = "${var.clientSecret}"
-  tenant_id       = "${var.tenantId}"
-  features {}
-}
